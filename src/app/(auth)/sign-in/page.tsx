a342729c46d9186bf3cf50fd51598d9f9729d165
sagarr sharmaa <sagarr2174@gmail.com>
2025-09-03 20:22:37 +0530

    Update page.tsx

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,10 +1,13 @@
- import { Card } from "@/components/ui/card"
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
-import { Sign } from "crypto";
 
 const page = () => {
-  console.log("sign in page");
-  return <SignInView />;  
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4 bg-gray-50">
+      <div className="w-full max-w-4xl">
+        <SignInView />
+      </div>
+    </div>
+  );
+}
 
-} 
 export default page;
