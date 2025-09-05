64c15c7f5a833fe78d2edfb5aede8e4c3f2575f3
sagarr sharmaa <sagarr2174@gmail.com>
2025-09-06 00:33:35 +0530

    Add files via upload

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,16 @@
+import { HomeView } from "@/modules/home/ui/views/home-view"
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+const page = async () => {
+const session = await auth.api.getSession({
+  headers: await headers(), 
+});
+  if(!session) {
+  redirect("/sign-in");
+  }
+
+  return <HomeView />
+};
+export default page;
